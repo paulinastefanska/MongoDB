c8f0fcb7dec55815b972baddd821ba8c6b72212e
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-24 21:47:48 +0200

    Add base adress MongoDB Atlas

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb+srv://dbUser:dbUserPassword@cluster0-fefl4.mongodb.net/test?retryWrites=true&w=majority', {
     useMongoClient: true
 });
 
